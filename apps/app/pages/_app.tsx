e368daca055c27b390548a4c59e6140e5df720dd
inglkruiz <12603425+inglkruiz@users.noreply.github.com>
2022-09-12 17:03:42 +0200

    chore(app): configure tailwindcss

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -8,7 +8,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to app!</title>
       </Head>
-      <main className="app">
+      <main>
         <Component {...pageProps} />
       </main>
     </>
